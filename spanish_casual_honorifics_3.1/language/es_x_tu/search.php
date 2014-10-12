07019b89120a66bed877be1e98735bcfee2cd8ec
phpBB España <thekuka@gmail.com>
2014-10-12 20:59:33 +0200

    Update search.php
    Casual listo...

--- spanish_casual_honorifics_3.1/language/es_x_tu/search.php
+++ spanish_casual_honorifics_3.1/language/es_x_tu/search.php
@@ -54,28 +54,28 @@ $lang = array_merge($lang, array(
 	'GLOBAL'				=> 'Anuncio Global',
 
 	'IGNORED_TERMS'			=> 'Ignorado',
-	'IGNORED_TERMS_EXPLAIN'	=> 'Las siguientes palabras en su consulta fueron ignoradas porque son palabras demasiado comunes: <strong>%s</strong>',
+	'IGNORED_TERMS_EXPLAIN'	=> 'Las siguientes palabras en tu consulta fueron ignoradas porque son palabras demasiado comunes: <strong>%s</strong>',
 
 	'JUMP_TO_POST'			=> 'Saltar al mensaje',
 
-	'LOGIN_EXPLAIN_EGOSEARCH'	=> 'El foro requiere que esté registrado e identificado para ver sus propios mensajes.',
-	'LOGIN_EXPLAIN_NEWPOSTS'	=> 'El foro requiere que esté registrado e identificado para ver nuevos mensajes desde su última visita.',
-	'LOGIN_EXPLAIN_UNREADSEARCH'=> 'El foro requiere que esté registrado e identificado para ver sus mensajes sin leer.',
+	'LOGIN_EXPLAIN_EGOSEARCH'	=> 'El foro requiere que estés registrado e identificado para ver tus propios mensajes.',
+	'LOGIN_EXPLAIN_NEWPOSTS'	=> 'El foro requiere que estés registrado e identificado para ver nuevos mensajes desde tu última visita.',
+	'LOGIN_EXPLAIN_UNREADSEARCH'=> 'El foro requiere que esté registrado e identificado para ver tus mensajes sin leer.',
 
 	'MAX_NUM_SEARCH_KEYWORDS_REFINE'	=> array(
-		1	=> 'Especificó demasiadas palabras a buscar. Por favor, no inserte más de %1$d palabra.',
-		2	=> 'Especificó demasiadas palabras a buscar. Por favor, no inserte más de %1$d palabras.',
+		1	=> 'Has especificado demasiadas palabras a buscar. Por favor, no insertes más de %1$d palabra.',
+		2	=> 'Has especificado demasiadas palabras a buscar. Por favor, no insertes más de %1$d palabras.',
 	),
 
-	'NO_KEYWORDS'			=> 'Debe especificar al menos una palabra para buscar. Cada palabra debe tener al menos %s y no debe contener más de %s excluyendo los comodines.',
+	'NO_KEYWORDS'			=> 'Debes especificar al menos una palabra para buscar. Cada palabra debe tener al menos %s y no debe contener más de %s excluyendo los comodines.',
 	'NO_RECENT_SEARCHES'	=> 'No se han realizado búsquedas recientemente',
-	'NO_SEARCH'				=> 'Disculpe, no le está permitido usar el sistema de búsqueda.',
+	'NO_SEARCH'				=> 'Disculpa, no te está permitido usar el sistema de búsqueda.',
 	'NO_SEARCH_RESULTS'		=> 'No se encontraron coincidencias.',
 	'NO_SEARCH_TIME'		=> array(
-		1	=> 'Disculpe, no puede buscar en este momento. Por favor, intente nuevamente en %d segundo.',
-		2	=> 'Disculpe, no puede buscar en este momento. Por favor, intente nuevamente en %d segundos.',
+		1	=> 'Disculpa, no puedes buscar en este momento. Por favor, inténtalo nuevamente en %d segundo.',
+		2	=> 'Disculpa, no puedes buscar en este momento. Por favor, inténtalo nuevamente en %d segundos.',
 	),
-	'NO_SEARCH_UNREADS'		=> 'Disculpe, pero la búsqueda de mensajes no leídos ha sido deshabilitada en este foro.',
+	'NO_SEARCH_UNREADS'		=> 'Disculpa, pero la búsqueda de mensajes no leídos ha sido deshabilitada en este foro.',
 	'WORD_IN_NO_POST'		=> 'La palabra <strong>%s</strong> no se encuentra en ningún mensaje.',
 	'WORDS_IN_NO_POST'		=> 'Las palabras <strong>%s</strong> no se encuentran en ningún mensaje.',
 
@@ -94,12 +94,12 @@ $lang = array_merge($lang, array(
 	'SEARCH_ALL_TERMS'			=> 'Buscar todos los términos',
 	'SEARCH_ANY_TERMS'			=> 'Buscar cualquier término',
 	'SEARCH_AUTHOR'				=> 'Buscar autor',
-	'SEARCH_AUTHOR_EXPLAIN'			=> 'Emplee * como comodín para coincidencias parciales.',
+	'SEARCH_AUTHOR_EXPLAIN'			=> 'Emplea * como comodín para coincidencias parciales.',
 	'SEARCH_FIRST_POST'			=> 'Solo el primer mensaje de los temas',
 	'SEARCH_FORUMS'				=> 'Buscar en Foros',
-	'SEARCH_FORUMS_EXPLAIN'			=> 'Seleccione el Foro o Foros en los que desea buscar. Para agilizar puede buscar en los subforos seleccionando el Foro padre y habilitar la búsqueda en los subforos (en Opciones de búsqueda).',
+	'SEARCH_FORUMS_EXPLAIN'			=> 'Selecciona el Foro o Foros en los que deseas buscar. Para agilizar puedes buscar en los subforos seleccionando el Foro padre y habilitar la búsqueda en los subforos (en Opciones de búsqueda).',
 	'SEARCH_IN_RESULTS'			=> 'Buscar en los resultados',
-	'SEARCH_KEYWORDS_EXPLAIN'		=> 'Escriba <strong>+</strong> delante de una palabra a encontrar y <strong>-</strong> delante de la palabra para excluirla. Crea una lista de palabras separadas por <strong>|</strong> entre corchetes si solo una de ellas se quiere encontrar. Emplee <strong>*</strong> como comodín para coincidencias parciales.',
+	'SEARCH_KEYWORDS_EXPLAIN'		=> 'Escribe <strong>+</strong> delante de una palabra a encontrar y <strong>-</strong> delante de la palabra para excluirla. Crea una lista de palabras separadas por <strong>|</strong> entre corchetes si solo una de ellas se quiere encontrar. Emplea <strong>*</strong> como comodín para coincidencias parciales.',
 	'SEARCH_MSG_ONLY'			=> 'Solo el texto del mensaje',
 	'SEARCH_OPTIONS'			=> 'Opciones de búsqueda',
 	'SEARCH_QUERY'				=> 'Consulta',
@@ -117,7 +117,7 @@ $lang = array_merge($lang, array(
 	'SPHINX_SEARCH_FAILED_LOG'	=> 'Lo sentimos, pero no se pudo realizar la búsqueda. Más información acerca de este fallo en el registro de errores.',
 
 	'TOO_FEW_AUTHOR_CHARS'	=> array(
-		1	=> 'Debe especificar al menos %d carácter del nombre del autor.',
-		2	=> 'Debe especificar al menos %d caracteres del nombre del autor.',
+		1	=> 'Debes especificar al menos %d carácter del nombre del autor.',
+		2	=> 'Debes especificar al menos %d caracteres del nombre del autor.',
 	),
 ));
