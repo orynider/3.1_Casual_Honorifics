991fa99aaca97c3bf23cda14b40d98a42ff6ecd1
phpBB España <thekuka@gmail.com>
2014-10-26 15:41:10 +0100

    Update phpBB 3.1 RC6

--- spanish_casual_honorifics_3.1/language/es_x_tu/acp/ban.php
+++ spanish_casual_honorifics_3.1/language/es_x_tu/acp/ban.php
@@ -42,30 +42,30 @@ $lang = array_merge($lang, array(
 	'30_MINS'			=> '30 minutos',
 	'6_HOURS'			=> '6 horas',
 
-	'ACP_BAN_EXPLAIN'		=> 'Aquí puede controlar las exclusiones de usuarios por nombre, IP o dirección de email. Este método impide a un usuario acceder a cualquier parte del sitio. Si quiere puede dar una razón breve (255 caracteres) para la exclusión. Se mostrará en el registro del Administrador. También puede especificar la duración de la exclusión. Si quiere que la exclusión termine en una fecha específica en lugar de después de un periodo de tiempo, seleccione <u>Hasta</u> en la duración de la exclusión e ingrese la fecha con el formato aaaa-mm-dd.',
+	'ACP_BAN_EXPLAIN'		=> 'Aquí puedes controlar las exclusiones de usuarios por nombre, IP o dirección de email. Este método impide a un usuario acceder a cualquier parte del sitio. Si quieres, puedes dar una razón breve (255 caracteres) para la exclusión. Se mostrará en el registro del Administrador. También puedes especificar la duración de la exclusión. Si quieres que la exclusión termine en una fecha específica en lugar de después de un periodo de tiempo, selecciona <u>Hasta</u> en la duración de la exclusión e ingresa la fecha con el formato aaaa-mm-dd.',
 
 	'BAN_EXCLUDE'			=> 'Quitar exclusión',
 	'BAN_LENGTH'			=> 'Duración de la exclusión',
 	'BAN_REASON'			=> 'Razón de la exclusión',
 	'BAN_GIVE_REASON'		=> 'Explicación de la razón de la exclusión',
-	'BAN_UPDATE_SUCCESSFUL'		=> 'La lista de exclusiones ha sido actualizada con éxito.',
+	'BAN_UPDATE_SUCCESSFUL'		=> 'La lista de exclusiones ha sido actualizada correctamente.',
 	'BANNED_UNTIL_DATE'		=> 'hasta %s', // Ejemplo: "hasta Lun 13.Jul.2009, 14:44"
 	'BANNED_UNTIL_DURATION'		=> '%1$s (hasta %2$s)', // Ejemplo: "7 días (hasta Mar 14.Jul.2009, 14:44)"
 
 	'EMAIL_BAN'			=> 'Excluir una o más direcciones de email',
-	'EMAIL_BAN_EXCLUDE_EXPLAIN'	=> 'Habilite esto para quitar la dirección de email ingresada de todos las exclusiones actuales.',
-	'EMAIL_BAN_EXPLAIN'		=> 'Para expecificar más de una dirección de email ingrese cada una en una nueva línea. Para direcciones parciales use * como comodín, p.ej. <samp>*@hotmail.com</samp>, <samp>*@*.domain.tld</samp>, etc.',
+	'EMAIL_BAN_EXCLUDE_EXPLAIN'	=> 'Habilita esto para quitar la dirección de email ingresada de todos las exclusiones actuales.',
+	'EMAIL_BAN_EXPLAIN'		=> 'Para expecificar más de una dirección de email, ingresa cada una en una nueva línea. Para direcciones parciales usa * como comodín, por ejemplo, <samp>*@hotmail.com</samp>, <samp>*@*.domain.tld</samp>, etc.',
 	'EMAIL_NO_BANNED'		=> 'No hay direcciones de email excluidas',
 	'EMAIL_UNBAN'			=> 'Levantar exclusión de emails',
-	'EMAIL_UNBAN_EXPLAIN'		=> 'Puede quitar la exclusión a múltiples direcciones de email al mismo tiempo usando la combinación apropiada de ratón y teclado para su navegador (p.ej. Ctrl+Clic). Las direcciones de email excluidas están en negrita.',
+	'EMAIL_UNBAN_EXPLAIN'		=> 'Puedes quitar la exclusión a múltiples direcciones de email al mismo tiempo usando la combinación apropiada de ratón y teclado en tu navegador (por ejemplo, Ctrl+Clic). Las direcciones de email excluidas están en negrita.',
 
 	'IP_BAN'			=> 'Excluir una o más IPs',
 	'IP_BAN_EXCLUDE_EXPLAIN'	=> 'Habilite esto para quitar la IP ingresada de todas las exclusiones actuales.',
-	'IP_BAN_EXPLAIN'		=> 'Para expecificar más de una IP ingrese cada una en una nueva línea. Para especificar un rango de direcciones IP separe el inicio y el final con un guión (-), para especificar un comodín use *',
+	'IP_BAN_EXPLAIN'		=> 'Para expecificar más de una IP ingrese cada una en una nueva línea. Para especificar un rango de direcciones IP separa el inicio y el final con un guión (-), para especificar un comodín usa *',
 	'IP_HOSTNAME'			=> 'Direcciones IP o hostnames',
 	'IP_NO_BANNED'			=> 'No hay direcciones IP excluidas',
 	'IP_UNBAN'			=> 'Levantar exclusión de IPs',
-	'IP_UNBAN_EXPLAIN'		=> 'Puede quitar la exclusión a múltiples direcciones IP al mismo tiempo usando la combinación apropiada de ratón y teclado para su navegador (p.ej. Ctrl+Clic). Las direcciones IP excluidas están en negrita.',
+	'IP_UNBAN_EXPLAIN'		=> 'Puedes quitar la exclusión a múltiples direcciones IP al mismo tiempo usando la combinación apropiada de ratón y teclado en tu navegador (por ejemplo, Ctrl+Clic). Las direcciones IP excluidas están en negrita.',
 
 	'LENGTH_BAN_INVALID'		=> 'La fecha debe tener el formato <kbd>AAAA-MM-DD</kbd>.',
 
@@ -76,10 +76,10 @@ $lang = array_merge($lang, array(
 
 	'UNTIL'						=> 'Hasta',
 	'USER_BAN'					=> 'Excluir uno o más usuarios por nombre de usuario',
-	'USER_BAN_EXCLUDE_EXPLAIN'	=> 'Habilite esto para quitar el nombre de usuario ingresado de todos las exclusiones actuales.',
-	'USER_BAN_EXPLAIN'			=> 'Para especificar más de un nombre de usuario ingresa cada uno en una nueva línea. Use el enlace <u>Buscar un usuario</u> para encontrar y añadir uno o más nombres de usuarios automáticamente.',
+	'USER_BAN_EXCLUDE_EXPLAIN'	=> 'Habilita esto para quitar el nombre de usuario ingresado de todos las exclusiones actuales.',
+	'USER_BAN_EXPLAIN'			=> 'Para especificar más de un nombre de usuario, ingresa cada uno en una nueva línea. Usa el enlace <u>Buscar un usuario</u> para encontrar y añadir uno o más nombres de usuarios automáticamente.',
 	'USER_NO_BANNED'			=> 'No hay nombres de usuario excluidos',
 	'USER_UNBAN'				=> 'Levantar exclusión a usuarios por nombres de usuario',
-	'USER_UNBAN_EXPLAIN'		=> 'Puede quitar la exclusión a múltiples nombres de usuario al mismo tiempo usando la combinación apropiada de ratón y teclado para su navegador (p.ej. Ctrl+Clic). Los nombres de usuario <em>excluidos</em> están en negrita.',
+	'USER_UNBAN_EXPLAIN'		=> 'Puede quitar la exclusión a múltiples nombres de usuario al mismo tiempo usando la combinación apropiada de ratón y teclado en tu navegador (por ejemplo Ctrl+Clic). Los nombres de usuario <em>excluidos</em> están en negrita.',
 
 ));
